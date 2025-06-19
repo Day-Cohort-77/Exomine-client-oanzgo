d409d39f956facb31962b6e28dceb664c1e334e5
Michael Phillips <mphillips242001@outlook.com>
2025-06-19 09:45:41 -0500

    main files initialized

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+
+
+
+const container = document.querySelector("#container");
+
+
+const render = async () => {
+  //const govenorHTML = await 
+ 
+
+
+  const composedHTML = `
+        <h1>The GMA</h1>
+
+
+        <article id="flex">
+            <section class="govenors">
+                <h2>Govenors</h2>
+                {govenorHTML}
+            </section>
+
+
+            <section>
+                <h2></h2>
+             
+            </section>
+
+
+            <section>               
+            <h2></h2>
+                
+            </section>
+
+
+            <section>
+                <h2></h2>
+               
+            </section>
+        </article>
+
+
+        <article>
+            
+        </article>
+
+
+        <article>
+            <h2></h2>
+        </article>
+    `;
+
+
+  container.innerHTML = composedHTML;
+};
+
+render()
