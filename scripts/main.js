df5573f6dc5c7a279fdc618332807513b7480391
Stephen <peterson.stephen138@gmail.com>
2025-06-23 15:06:29 -0500

    notes added to mining facility

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 import { governorsOptions } from "./governors.js";
 import { facilityOptions, chooseMineral } from "./miningFacility.js";
 import { OrderButton } from "./orderButton.js";
-;
-const container = document.querySelector("#container");
+
+const container = document.querySelector("#container");//We declare a new variable and give its value the container id within our index.HTML
 
 
-const render = async () => {
-    const governorHTML = await governorsOptions()
+const render = async () => {//declared a variable render, gave it the value of an async function
+    const governorHTML = await governorsOptions()//we create a new variable that invokes our governorsOptions function, but by using AWAIT, will only give the return of that function once our promises are cleared.
     const facilityHTML = await facilityOptions()
-    const radioButtonsHTML = await chooseMineral()
-    const buttonHTML = await OrderButton()
-
-  const composedHTML = `
+    const buttonHTML =  OrderButton()
+    const mineralHTML = await chooseMineral()
+  const composedHTML = //new variable that holds our HTML. we use this to build the framework of our basic html and make section tags to be able to add in our variables that invoke our option functions to insert our innerHTML.
+  ` 
         <h1>The GMA</h1>
 
 
@@ -25,12 +25,13 @@ const render = async () => {
             <section>
                 <h2>Mining Facilities</h2>
              ${facilityHTML}
+             ${mineralHTML}
             </section>
 
 
             <section>               
             <h2></h2>
-                ${radioButtonsHTML}
+                ${buttonHTML}
             </section>
 
 
@@ -52,9 +53,9 @@ const render = async () => {
     `;
 
 
-  container.innerHTML = composedHTML;
+  container.innerHTML = composedHTML;//we are selecting our previously defined variable container and selecting ONLY its innerHTML attribute. we then set that attribute's value to our html string
 };
 
-document.addEventListener("stateChanged", render)
+document.addEventListener("stateChanged", render)//we add an event listener to listen for our stateChanged custom event, and on that change of state, update our render function.
 
 render()
